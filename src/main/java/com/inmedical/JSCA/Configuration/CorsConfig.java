8418db34e48a2d2f5b38f09ab99c7be7d6b9c8e0
Sebas <elsebaslml@gmail.com>
2024-08-01 23:55:46 -0500

    BackEnd 1.5

---
+++ src/main/java/com/inmedical/JSCA/Configuration/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.inmedical.JSCA.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://placasfront-production.up.railway.app"); // Tu dominio de frontend
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
